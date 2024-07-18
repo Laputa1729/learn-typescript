df91a85ab3bd51760ee359c1427f597f5da15e58
Laputa <Laputa1729@163.com>
2024-07-18 23:08:14 +0800

    Chore.

--- chapter01/04_types.ts
+++ chapter01/04_types.ts
@@ -21,3 +21,59 @@ let d: (a: number, b: number) => number;
 d = function (n1, n2, n3) {
   return n1 + n2;
 }
+
+/*
+* 数组的类型声明
+* 语法：
+* 类型[]
+* Array<类型>
+* */
+// string[] 表示字符串数组
+let e: string[];
+e = ['a', 'b', 'c'];
+
+let f: number[];
+
+let g: Array<number>;
+g = [1, 2, 3];
+
+/*
+* 元组，就是固定长度的数组
+* 语法：
+* [类型, 类型, ...]
+* */
+let h: [string, number];
+h = ['hello', 123];
+
+/*
+* enum 枚举类型
+* */
+let i: { name: string, gender: 0 | 1 };
+i = {
+  name: '孙悟空',
+  gender: 1
+};
+
+enum Gender {
+  Male = 0,
+  Female = 1
+}
+
+let i1: { name: string, gender: Gender };
+i1 = {
+  name: '孙悟空',
+  gender: Gender.Male
+};
+console.log('@@@', i1.gender === Gender.Male);
+
+// & 表示必须同时满足两个类型要求
+let j: { name: string } & { age: number };
+j = {
+  name: '孙悟空',
+  age: 18
+};
+
+// 类型的别名
+type myType = 1 | 2 | 3 | 4 | 5;
+let k: myType;
+k = 1;
